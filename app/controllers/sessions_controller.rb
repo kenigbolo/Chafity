2745f1d030bf3a3d0e427afd5f474f8e0c355385
kenigbolo <kenigbol@ut.ee>
2016-09-19 00:12:42 +0300

    Add linkedin authentication on omniauth end

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+  
+  def create
+  	raise env["omniauth.auth"].to_yaml
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root 'home#index'
-
+  get 'auth/:provider/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
