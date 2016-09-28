b5f2822c0898ddb65b876a84015361cf964f93bc
Kennu76 <kevin.kanarbik@gmail.com>
2016-09-28 19:02:49 +0300

    made about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users #, only: [:show, :edit, :update, :destroy]
   get 'auth/:provider/callback', to: 'sessions#create'
   root 'home#index'
+  get '/about', to: 'home#about'
 
 
 
