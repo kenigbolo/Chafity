916ec9a0a4456c699dd5ee17292a15f94f71268a
Fortunat Mutunda <fotimutunda@gmail.com>
2016-10-08 14:25:45 +0300

    fixed the registration image bug and edited some of the rigistration form

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,9 +3,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
   	user = User.from_omniauth(request.env["omniauth.auth"])
   	if user.persisted?
-  	  # Redirect to user profile
-			byebug
-  	  sign_in_and_redirect user, notice: "Signed in!"
+  	  sign_in_and_redirect edit_user_registration, notice: "Please update your account"
   	else
   	  redirect_to new_user_registration_url
   	  session["devise.user_attributes"] = user.user_attributes
