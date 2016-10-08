3852e126e3a11f88e34f7b8fbf3dcc621db533e3
Fortunat Mutunda <fotimutunda@gmail.com>
2016-10-08 14:48:12 +0300

    added messages into dashboard start

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
   	user = User.from_omniauth(request.env["omniauth.auth"])
   	if user.persisted?
-  	  sign_in_and_redirect edit_user_registration, notice: "Please update your account"
+  	  sign_in_and_redirect user, notice: "Signed in!"
   	else
   	  redirect_to new_user_registration_url
   	  session["devise.user_attributes"] = user.user_attributes
