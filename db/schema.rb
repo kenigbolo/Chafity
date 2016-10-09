f4311e3fd7c65ba701874ac8a82b9af3c2e44bad
kenigbolo <kenigbol@ut.ee>
2016-10-09 21:50:05 +0300

    Remove charities table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008175146) do
+ActiveRecord::Schema.define(version: 20161009184758) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.string   "message_body"
+    t.string   "message_status"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -77,8 +86,8 @@ ActiveRecord::Schema.define(version: 20161008175146) do
     t.string   "phone"
     t.string   "industry"
     t.string   "slug"
-    t.decimal  "donation_amount",        default: "0.0"
     t.decimal  "total_donated",          default: "0.0"
+    t.decimal  "donation_amount",        default: "5.0"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
