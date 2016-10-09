b6ec313cb383a2ae08647d479f61bef0c390581c
kenigbolo <kenigbol@ut.ee>
2016-10-09 20:20:52 +0300

    Show message body only when user has made payment

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,28 @@
 class ChargesController < ApplicationController
-	def new
-	end
+  def new
+  end 
 
-	def create
-	  @amount = (params[:amount].to_f * 100).to_i
+  def create
+    @amount = (params[:amount].to_f * 100).to_i
 
-	  customer = Stripe::Customer.create(
-	    email: params[:stripeEmail],
-	    source: params[:stripeToken]
-	  )
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
 
-	  charge = Stripe::Charge.create(
-	    customer: customer.id,
-	    amount: @amount,
-	    description: params[:desc],
-	    currency: 'eur'
-	  )
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: params[:desc],
+      currency: 'eur'
+    )
+    
+    session["payment"] = charge.status
+    redirect_to user_path(params[:id])
+  
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to root_path
+  end
 
-	rescue Stripe::CardError => e
-	  flash[:error] = e.message
-	  redirect_to new_charge_path
-	end
 end
