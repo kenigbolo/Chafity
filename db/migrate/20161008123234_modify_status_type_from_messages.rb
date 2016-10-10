cc7e9d13a9177953881846d670595144479c4340
kenigbolo <kenigbol@ut.ee>
2016-10-10 10:53:29 +0300

    Modify master branch for PG production environment

--- db/migrate/20161008123234_modify_status_type_from_messages.rb
+++ db/migrate/20161008123234_modify_status_type_from_messages.rb
@@ -1,5 +1,5 @@
 class ModifyStatusTypeFromMessages < ActiveRecord::Migration[5.0]
   def change
-    change_column :messages, :status, :boolean
+    change_column :messages, :status, 'boolean USING CAST(status AS boolean)'
   end
 end
