5aa6503fe4e5444e01e60db934c50b9b3fc046ad
kenigbolo <kenigbol@ut.ee>
2016-10-19 12:01:35 +0300

    Modify master branch to reflect changes from development branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,4 +68,11 @@ class User < ApplicationRecord
     end
   end
 
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
 end
--- db/migrate/20161008123234_modify_status_type_from_messages.rb
+++ db/migrate/20161008123234_modify_status_type_from_messages.rb
@@ -1,5 +1,9 @@
 class ModifyStatusTypeFromMessages < ActiveRecord::Migration[5.0]
   def change
-    change_column :messages, :status, 'boolean USING CAST(status AS boolean)'
+  	if ActiveRecord::Base.connection.adapter_name == 'postgresql'
+    	change_column :messages, :status, 'boolean USING CAST(status AS boolean)'
+    else
+    	change_column :messages, :status, :boolean
+    end
   end
 end
