711132f3dd326351f686632b8ba7de036fef64eb
kenigbolo <kenigbol@ut.ee>
2016-10-19 11:47:26 +0300

    Fix #16 - Tests for user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010040616) do
+ActiveRecord::Schema.define(version: 20161010073130) do
 
   create_table "charities", force: :cascade do |t|
     t.string   "name"
