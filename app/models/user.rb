9e566d0e01b18cd5ea66971f61c92aae58f21c9f
Fortunat Mutunda <fotty@Fortunats-MacBook-Pro.local>
2016-10-23 04:44:31 +0300

    added pg search to allow the user all the search required, now autocomplition and sidebar search remaining  #44

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,35 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  include PgSearch
   extend FriendlyId
-  friendly_id :slug_candidates , use: [:slugged, :finders]
 
+  friendly_id :slug_candidates , use: [:slugged, :finders]
   mount_uploader :image, AvatarUploader
-  
+  pg_search_scope :search,
+                   against: [
+                     :first_name, :last_name, :email,:location,:headline,:industry,
+                     :description
+                   ],
+                   associated_against: {
+                     charity: [:name, :address, :country]
+                     }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   validates_uniqueness_of :email
+  has_many :messages
+  belongs_to :charity
 
   before_save do
     self.first_name.capitalize
     self.last_name.capitalize
   end
 
-
-  has_many :messages
-  belongs_to :charity
-
-  def should_generate_new_friendly_id?
-    false if Rails.env.production?
-  end
+  # def should_generate_new_friendly_id?
+  #   false if Rails.env.production?
+  # end
 
   def self.from_omniauth(auth)
   	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -59,17 +66,8 @@ class User < ApplicationRecord
   def slug_candidates
     [
         [:first_name, :last_name],
-        [:first_name, :last_name,self.id]
+        [:first_name, :last_name,:id]
     ]
-  end
-
-# TODO: this method is to be update as well
-	def self.search(search)
-    if search
-      User.where('first_name Like ? OR last_name Like ?', search.capitalize, search.capitalize)
-    else
-      User.all
-    end
   end
 
   def update_with_password(params, *options)
@@ -79,5 +77,5 @@ class User < ApplicationRecord
       super
     end
   end
-    
+
 end
