6c6f67bf5d7b787cfe4dbc9ca54987d0f70afac3
Kennu76 <kevin.kanarbik@gmail.com>
2016-10-25 15:09:37 +0300

    it works!

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,20 @@ require 'factory_girl'
 10.times do
 user = FactoryGirl.build(:user)
 
-user_id = Random.rand(10)
+user_id = Random.rand(1..10)
 User.create! email: user.email , password: user.password,first_name: user.first_name, last_name: user.last_name,
              location: user.location, headline: user.headline, description: user.description, image: user.image,
              industry: user.industry
 
-charity = FactoryGirl.build(:charity)
-Charity.create! name: charity.name, address: charity.address, total_amount: charity.total_amount,
-                iban: charity.iban, registration_number: charity.registration_number, country: charity.country , user_id: user_id
+# charity = FactoryGirl.build(:charity)
+# Charity.create! name: charity.name, address: charity.address, total_amount: charity.total_amount,
+#                 iban: charity.iban, registration_number: charity.registration_number, country: charity.country , user_id: user_id
+
+
+# message = FactoryGirl.build(:message)
+# Message.create!  created_at: message.created_at, updated_ad: message.updated_at, sender_id: message.sender_id,
+#                  receiver_id: message.receiver_id, message_body: message.message_body, appointment_date: message.appointment_date,
+#                  user_id: message.user_id
 
 end
 puts "Seed was successful"
