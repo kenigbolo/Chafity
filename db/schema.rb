33ce27d84d86ac3de2d91efca7755d24f9cfe6e2
Fortunat Mutunda <fotimutunda@gmail.com>
2016-10-25 20:00:18 +0300

    fixed a bug on the search engine

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010073130) do
+ActiveRecord::Schema.define(version: 20161024203637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "charities", force: :cascade do |t|
     t.string   "name"
