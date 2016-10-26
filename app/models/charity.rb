318f81863393e96001e6b244bf54ef34b32e45c9
Kennu76 <kevin.kanarbik@gmail.com>
2016-10-26 20:13:11 +0300

    validates

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -1,4 +1,12 @@
 class Charity < ApplicationRecord
   has_many :users
-  validates :name, :address, :total_amount, :iban, :registration_number, :country ,presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :total_amount, presence: true
+  validates :iban, presence: true
+  validates :registration_number, presence: true
+  validates :country, presence: true
+  validates :created_at, presence: true
+  validates :updated_at, presence: true
+  validates :user_id, presence: true
 end
