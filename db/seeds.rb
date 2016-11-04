5263f57231bf44bfe74445ffdb0b410561f470d8
Fortunat Mutunda <fotimutunda@gmail.com>
2016-11-04 19:25:04 +0200

    fixed uI issues and merge conflicts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-if RAILS_ENV['production']
+if ENV['RAILS_ENV'] = 'production'
     Charity.create(id: 1, name: 'Golden Minds', address: 'W.Struve 1, Tartu', total_amount: 15_243,
                    iban: 'EE26819173836287161', registration_number: 'U78GH897XC', country: 'Estonia')
 else
