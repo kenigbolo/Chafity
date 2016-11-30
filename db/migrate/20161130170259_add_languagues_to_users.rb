740638c80f3e2e9972ff3ef06457a0553e7391c7
Fortunat Mutunda <fotimutunda@gmail.com>
2016-11-30 19:17:02 +0200

    Ninja migration

--- db/migrate/20161130170259_add_languagues_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLanguaguesToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :languages, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105164117) do
+ActiveRecord::Schema.define(version: 20161130171339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
