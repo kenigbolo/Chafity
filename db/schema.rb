56558ed49e3eacf65286a04d8895c51bf533c414
kenigbolo <kenigbol@ut.ee>
2016-11-30 20:05:54 +0200

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,9 +1,19 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20161130171339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_trgm"
 
   create_table "charities", force: :cascade do |t|
     t.string   "name"
@@ -53,15 +63,6 @@ ActiveRecord::Schema.define(version: 20161130171339) do
     t.index ["user_id"], name: "index_payments_on_user_id", using: :btree
   end
 
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text     "content"
-    t.string   "searchable_type"
-    t.integer  "searchable_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
-  end
-
   create_table "responses", force: :cascade do |t|
     t.text     "body"
     t.integer  "message_id"
@@ -106,6 +107,7 @@ ActiveRecord::Schema.define(version: 20161130171339) do
     t.decimal  "donation_amount",        default: "5.0"
     t.integer  "charity_id"
     t.boolean  "admin",                  default: false, null: false
+    t.string   "languages"
     t.index ["charity_id"], name: "index_users_on_charity_id", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
