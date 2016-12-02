acc6f028d4c0d6592af0c5cfb7738b95d7f3539e
kenigbolo <kenigbol@ut.ee>
2016-12-02 12:46:55 +0200

    Modify tests cases for Mailer -> Refactor for Red to Green

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,31 +1,33 @@
 require "rails_helper"
 
-RSpec.describe UserMailerMailer, type: :mailer do
+RSpec.describe UserMailer, type: :mailer do
   describe "welcome_confirmation" do
-    let(:mail) { UserMailerMailer.welcome_confirmation }
+    let(:user) { FactoryGirl.build :user}
+    let(:mail) { UserMailer.welcome_confirmation(user) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Welcome confirmation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("Welcome to Chafity!")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["notification@chafity.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match(user.to_s)
     end
   end
 
   describe "payment_confirmation" do
-    let(:mail) { UserMailerMailer.payment_confirmation }
+    let(:payment) {FactoryGirl.build :payment}
+    let(:mail) { UserMailer.payment_confirmation(payment) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Payment confirmation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("Payment status notification!")
+      expect(mail.to).to eq([payment.user.email])
+      expect(mail.from).to eq(["notification@chafity.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match(payment.user.to_s)
     end
   end
 
