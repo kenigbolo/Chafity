3e3268ab4c31cd05cee324593cb886a75ab25a23
kenigbolo <kenigbol@ut.ee>
2016-12-08 14:12:43 +0200

    Allow public uploads folder to be accessible by heroku since we currently do not have cloudinary space

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,8 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true
+  config.serve_static_assets = true
   config.log_level = :debug
   config.log_tags = [ :request_id ]
   config.action_mailer.perform_caching = false
