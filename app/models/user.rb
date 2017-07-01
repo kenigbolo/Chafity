7ba90fd8d328c5e88089e789259be4cd388c364c
kenigbolo <kenigbol@ut.ee>
2017-07-01 21:36:02 +0300

    REfactor method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,19 +76,7 @@ class User < ApplicationRecord
   end
 
   def update_with_password(params, *options)
-    if encrypted_password.blank?
-      update_attributes(params, *options)
-    else
-      super
-    end
-  end
-
-  def update_with_password(params, *options)
-    if encrypted_password.blank?
-      update_attributes(params, *options)
-    else
-      super
-    end
+    encrypted_password.blank? ? update_attributes(params, *options) : super
   end
 
   def has_paid_to_contact(user)
